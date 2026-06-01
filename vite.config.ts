d61881551805674ede4b940a2adf44c10ff1140f
Engr. Sulaiman <77010217+sulaimancodesnew@users.noreply.github.com>
2026-06-01 07:22:52 +0000

    Fix vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { nitro } from 'nitro/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [
     tanstackStart(),
     nitro(),
